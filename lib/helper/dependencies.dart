bf666a023b63b6e2ff3b7c5b8240eaa4e09a5a13
jeremy-tesh <jermitesh20@gmail.com>
2022-08-30 02:58:24 +0300

    helper modification

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,4 +1,5 @@
 import 'package:get/instance_manager.dart';
+import 'package:yehiwot_kal/controllers/lesson_data_controller.dart';
 import 'package:yehiwot_kal/data/api/api_client.dart';
 import 'package:yehiwot_kal/data/repository/lesson_repo.dart';
 
@@ -8,4 +9,7 @@ Future<void> init() async {
 
 //repos
   Get.lazyPut(() => LessonRepo(apiClient: Get.find()));
+
+//controllers
+  Get.lazyPut(() => LessonDataController(lessonRepo: Get.find()));
 }
