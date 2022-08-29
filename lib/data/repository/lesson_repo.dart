8c6e750a8304ccaf1f5c8ac2238846a81e61ebbb
jeremy-tesh <jermitesh20@gmail.com>
2022-08-30 01:48:36 +0300

    building controller

---
+++ lib/data/repository/lesson_repo.dart
@@ -0,0 +1,13 @@
+import 'package:get/get_connect.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:yehiwot_kal/data/api/api_client.dart';
+
+class LessonRepo extends GetxService {
+  final ApiClient apiClient; //api instance
+
+  LessonRepo({required this.apiClient});
+
+  Future<Response> getLessonDataList() async {
+    return await apiClient.getData("end point url");
+  }
+}
