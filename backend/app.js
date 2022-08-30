877e93f4a8b1f827b3cb0b10c47ecf07a40de3c1
jeremy-tesh <jermitesh20@gmail.com>
2022-08-30 18:53:24 +0300

    backend setup and db connection

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+//Import Route
+const lessonsRoute =  require('./routes/lessons')
+
+//middleware
+app.use('/lessons',lessonsRoute);
+
+//routes
+app.get('/',(req,res)=>{
+    res.send('Home')
+})
+
+
+//Database connection
+
+const uri =process.env.ATLAS_URI;
+// mongoose.connect(uri)
+// const connection = mongoose.connection;
+// connection.once('open',()=>{
+//     console.log("mongoDb success")
+// })
+
+mongoose.connect(uri,()=>console.log('db worked'))
+
+
+
+//listening to server on port 3000
+app.listen(3000)
