f8c88e5f12a6ee9685c72be184b39920593c332a
jeremy-tesh <jermitesh20@gmail.com>
2022-08-31 02:40:01 +0300

    fix:backend connection

--- lib/data/repository/lesson_repo.dart
+++ lib/data/repository/lesson_repo.dart
@@ -1,6 +1,7 @@
 import 'package:get/get_connect.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:yehiwot_kal/data/api/api_client.dart';
+import 'package:yehiwot_kal/utils/app_constants.dart';
 
 class LessonRepo extends GetxService {
   final ApiClient apiClient; //api instance
@@ -8,6 +9,6 @@ class LessonRepo extends GetxService {
   LessonRepo({required this.apiClient});
 
   Future<Response> getLessonDataList() async {
-    return await apiClient.getData("/lessons");
+    return await apiClient.getData(AppConstants.LESSON_DATA_URI);
   }
 }
--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -2,11 +2,11 @@ import 'package:get/instance_manager.dart';
 import 'package:yehiwot_kal/controllers/lesson_data_controller.dart';
 import 'package:yehiwot_kal/data/api/api_client.dart';
 import 'package:yehiwot_kal/data/repository/lesson_repo.dart';
+import 'package:yehiwot_kal/utils/app_constants.dart';
 
 Future<void> init() async {
 //api client
-  Get.lazyPut(
-      () => ApiClient(appBaseUrl: "https://wordoflifeapi.herokuapp.com"));
+  Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.BASE_URL));
 
 //repos
   Get.lazyPut(() => LessonRepo(apiClient: Get.find()));
